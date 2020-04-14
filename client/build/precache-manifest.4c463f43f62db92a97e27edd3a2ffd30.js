87efb27d3b3852e49a1483e2ad01184bd9ecbbf0
ADMIN <admin@QBs-MacBook-Pro.local>
2020-04-14 23:42:47 +0700

    add build dir

---
+++ client/build/precache-manifest.4c463f43f62db92a97e27edd3a2ffd30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dccb221aa764a36e02eee1845e18884",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c0ad666cbfeb84c00eb",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7948535788aa40788a5d",
+    "url": "/static/css/main.6383cd5b.chunk.css"
+  },
+  {
+    "revision": "7c0ad666cbfeb84c00eb",
+    "url": "/static/js/2.2ddd3ff5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2ddd3ff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7948535788aa40788a5d",
+    "url": "/static/js/main.dfd4267c.chunk.js"
+  },
+  {
+    "revision": "977bc0f2a2db2d178e88",
+    "url": "/static/js/runtime-main.66a8a0e9.js"
+  }
+]);
