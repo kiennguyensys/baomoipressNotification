1b6ece1728be9c47d21c6d353cf5e548752ed57d
ADMIN <admin@QBs-MacBook-Pro.local>
2020-04-15 00:20:14 +0700

    production handler

--- app.js
+++ app.js
@@ -16,11 +16,10 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(cors());
-app.use(logger("dev"));
+//app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/fcm-sender", fcmSenderRouter);
