bf959bd3e3d104a9c8ea2cbb8de4043512038de9
ADMIN <admin@QBs-MacBook-Pro.local>
2020-04-15 00:27:19 +0700

    remove jade view

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ var fcmCronRouter = require("./routes/fcmCron");
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
 app.use(cors());
 //app.use(logger("dev"));
 app.use(express.json());
