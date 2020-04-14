08ab70333ef331c1a86b96a8d6cc671888cd32a0
ADMIN <admin@QBs-MacBook-Pro.local>
2020-04-15 00:33:21 +0700

    change express app

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 
 app.use(cors());
 //app.use(logger("dev"));
-app.use(express.json());
+//app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
@@ -22,29 +22,26 @@ app.use("/fcm-sender", fcmSenderRouter);
 app.use("/fcm-cron", fcmCronRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-});
+// app.use(function(req, res, next) {
+//     next(createError(404));
+// });
 
 // error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+// app.use(function(err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
-});
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render("error");
+// });
 
 //production handler
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 module.exports = app;
